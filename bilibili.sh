1bf00c996a20dc73dc1482abde2a6d1928265f0f
Qiu Xiang <i@7c00.cc>
2017-05-12 16:41:03 +0800

    移除临时文件的使用

--- bilibili.sh
+++ bilibili.sh
@@ -3,8 +3,6 @@
 # (c) 2017 Qiu Xiang <i@7c00.cc> under MIT licence
 #
 
-ASS_FILE=/tmp/comments.ass
-COMMENTS_FILE=/tmp/comments.xml
 BASE_PATH=$(cd $(dirname $0); pwd)
 DANMAKU2ASS_PATH="$BASE_PATH/danmaku2ass.py"
 
@@ -18,13 +16,13 @@ main() {
   local video_pattern=https?://www.bilibili.com/video/av[0-9]+/
   local anime_pattern=https?://bangumi.bilibili.com/anime/[0-9]+/play#[0-9]+
   if [[ $1 =~ $anime_pattern ]]; then
-    echo 'Get episode data'
+    echo Get episode data
     local episode_id=${1#*#}
     local episode_data=$(request http://bangumi.bilibili.com/web_api/episode/$episode_id.json)
     local danmaku_id=$(jq -r .result.currentEpisode.danmaku <<< $episode_data)
     av_id=$(jq -r .result.currentEpisode.avId <<< $episode_data)
   elif [[ $1 =~ $video_pattern  ]]; then
-    av_id=$(expr "$1" : '.*av\(.*\)/')
+    av_id=$(expr "$1" : .*av\(.*\)/)
   else
     echo The url $1 is invalid.
     echo A valid url is like $video_pattern or
@@ -32,14 +30,14 @@ main() {
     exit 1
   fi
 
-  echo 'Get video data'
+  echo Get video data
   local video_data=$(request "http://api.bilibili.com/view?appkey=8e9fc618fbd41e28&id=$av_id")
   local cid=$(jq .cid <<< $video_data)
   local title=$(jq -r .title <<< $video_data)
   local params="_appver=424000&_device=android&_down=0&_hwid=$RANDOM&_p=1&_tid=0&appkey=452d3958f048c02a&cid=$cid&otype=json&platform=android"
   local sign=$(echo -n ${params}f7c926f549b9becf1c27644958676a21 | md5)
 
-  echo 'Get playlist'
+  echo Get playlist
   local play_url=$(request "https://interface.bilibili.com/playurl?$params&sign=${sign:0:32}")
   local length=$(jq ".durl | length" <<< $play_url)
   local playlist
@@ -47,10 +45,10 @@ main() {
     playlist+="$(jq -r .durl[${i}].url <<< $play_url) "
   done
 
-  echo 'Get comments'
-  request http://comment.bilibili.com/$cid.xml --compressed > $COMMENTS_FILE
-  danmaku2ass -s 1280x720 -dm 20 -o $ASS_FILE $COMMENTS_FILE
-  mpv --force-media-title "$title" -sub-file $ASS_FILE --merge-files $playlist
+  echo Get comments
+  local comments=$(request http://comment.bilibili.com/$cid.xml --compressed)
+  local ass=$(danmaku2ass -s 1280x720 -dm 20 -o >(cat) <(echo "$comments"))
+  mpv --force-media-title "$title" -sub-file <(echo "$ass") --merge-files $playlist
 }
 
 if [ -z $1 ]; then
